0fd34e767e529cfcb906494de651a6d95e600e73
redtubebzz <parvesads@gmail.com>
2026-04-30 23:00:04 +0600

    Update and rename index.js to index.html

--- index.js
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware(request) {
-  return NextResponse.redirect("https://motortape.com/pynxydkcf?key=9fcaaab1932732baaffa3314295d33af", 307);
-}
